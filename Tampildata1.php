62a1f83e19facd1bf9d5bea0efbf0f919a2a396c
bambangdwihartono <bambangdwihartono3@gmail.com>
2017-10-24 17:06:52 +0700

    komit ketiga

--- Tampildata1.php
+++ Tampildata1.php
@@ -18,18 +18,17 @@
  	foreach ($db->tampil_data() as $x) {
  	 ?>
 	 	<tr>
-	 		<td><?php echo $no; ?></td>
+	 		<td><?php echo $no++; ?></td>
 	 		<td><?php echo $x['nama']; ?></td>
 	 		<td><?php echo $x['alamat']; ?></td>
 	 		<td><?php echo $x['usia']; ?></td>
 	 		<td>
 	 			<a href="edit.php?id=<?php echo $s['id']?>&aksi=edit">Edit</a>
-	 			<a href="proses.php?id=<?php echo $s['id']?>&aksi=hapus">Edit</a>
+	 			<a href="proses.php?id=<?php echo $s['id']?>&aksi=hapus">Hapus</a>
 	 		</td>
 	 	</tr>
 	 <?php 
 
 	}
-
 	  ?>
  </table>
